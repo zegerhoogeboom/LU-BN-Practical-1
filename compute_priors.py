b62928eb360c8c4a0cbc3f8c61deaf60c0fd4999
Zeger Hoogeboom <zeger.hoogeboom@gmail.com>
2017-04-23 17:50:41 +0200

    Updated description to 14 values, removed dependency.

--- compute_priors.py
+++ compute_priors.py
@@ -27,7 +27,7 @@ valid_descriptions = ['PETIT LARCENY', 'HARRASSMENT 2', 'ASSAULT 3 & RELATED OFF
                       'CRIMINAL MISCHIEF & RELATED OF',
                       'GRAND LARCENY', 'OFF. AGNST PUB ORD SENSBLTY &', 'DANGEROUS DRUGS', 'FELONY ASSAULT', 'ROBBERY',
                       'MISCELLANEOUS PENAL LAW', 'BURGLARY', 'DANGEROUS WEAPONS',
-                      'OFFENSES AGNST PUBLIC ADMINISTRATION',
+                      'OFFENSES AGAINST PUBLIC ADMINI',
                       ]
 valid_jurisdiction = ['N.Y. POLICE DEPT', 'N.Y. HOUSING POLICE', 'N.Y. TRANSIT POLICE', 'PORT AUTHORITY',
                       'DEPT OF CORRECTONS']
@@ -89,18 +89,18 @@ def compute_prior_given_data3(column1, column2, column3, y):
 
 # Single dependencies:
 # compute_prior_given_data(df["time_slots"], df["location"])
-# compute_prior_given_data(df["description"], df["level_of_offense"])
+compute_prior_given_data(df["description"], df["level_of_offense"])
+compute_prior_given_data(df["description"], df["jurisdiction"])
 
 # Two dependencies:
 # compute_prior_given_data2(df["borough"], df["location"], df["premise"])
 # compute_prior_given_data2(df["borough"], df['location'], df["park"])
 # compute_prior_given_data2(df["borough"], df["park"], df["precinct"])
-# compute_prior_given_data2(df["description"], df["level_of_offense"], df["jurisdiction"])
 # compute_prior_given_data2(df["month"], df["day_of_week"], df["time_slots"])
 
 # Three dependencies:
 compute_prior_given_data3(df["premise"], df["park"], df["time_slots"], df["description"])
-# compute_prior_given_data3(df["level_of_offense"], df["description"], df["time_slots"], df["outcome"])
+compute_prior_given_data3(df["level_of_offense"], df["description"], df["time_slots"], df["outcome"])
 
 
 for column in df.columns:
